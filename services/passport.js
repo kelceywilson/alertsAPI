a94eaf94d2a98f6f590ee2709ff301a5f379e87e
Kelcey Wilson <kelceywilson@gmail.com>
2018-04-25 14:55:48 -0700

    Add explicit returns to jwtLogin

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport')
-const User = require('../db/models').User
+const { User } = require('../db/models')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
@@ -7,17 +7,14 @@ const LocalStrategy = require('passport-local')
 
 // Create local strategy
 const localOptions = { usernameField: 'email' }
+
 const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
-  // verify email/pw - correct give done the user - otherwise give it false
-  User.findOne({ email: email }, (err, user) => {
+  User.findOne({ email }, (err, user) => {
     if(err) { return done(err) }
-    if(!user) { return done(null, false)}
-    // compare pws
-    console.log('pw from local signin', password);
-    console.log('pw hash from db', user.password);
+    if(!user) { return done(null, false) }
     user.comparePassword(password, (err2, isMatch) => {
       if(err2) { return done(err2) }
-      if(!isMatch) { return done(null, false)}
+      if(!isMatch) { return done(null, false) }
       return done(null, user)
     })
   })
@@ -29,18 +26,17 @@ const jwtOptions = {
   secretOrKey: config.secret
 }
 
-// Create JWT strategy - payload is the token from tokenForUser
+// JWT strategy - payload is the token from tokenForUser
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
-// See if the user id in the payload exists in our database
-// If it does, call done with that user
-// otherwise call done without user Object
+  // See if the user id in the payload exists in our database
+  // If it does, call done with that user
+  // otherwise call done without user Object
   User.findById(payload.sub, function(err, user){
     if(err) { return done(err, false) }
     if (user) {
-      done(null, user)
-    } else {
-      done(null, false)
+      return done(null, user)
     }
+    return done(null, false)
   })
 })
 
