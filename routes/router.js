b467c00ca9f21b51c74116c090e7a9862cde4dd6
Kelcey Wilson <kelceywilson@gmail.com>
2018-04-27 12:36:48 -0700

    Modularized search query into db.js

--- routes/router.js
+++ routes/router.js
@@ -2,68 +2,64 @@ const express = require('express')
 const Authentication = require('../controllers/authentication')
 const passportService = require('../services/passport')
 const passport = require('passport')
-const { Alert } = require('../db/models.js')
-const {
-  addNewAlert,
-  deleteOneAlert,
-  getAllAlerts,
-  getOneAlert
-} = require('../db/db.js')
-
-/* CODE_REVIEW: Consider replacing methods above with :
+const { Alert } = require('../db/models')
 const db = require('../db/db')
-- will have to update db.js to do this.
-*/
 
 const requireAuth = passport.authenticate('jwt', { session: false })
 const requireSignin = passport.authenticate('local', { session: false })
 
 const router = express.Router()
 
-router.get('/', requireAuth, (req, res) => {
-  res.send({ message: 'superdooper'})
-})
+// POST signin and signup using Passport authentication
 router.post('/signin', requireSignin, Authentication.signin)
 router.post('/signup', Authentication.signup)
 
 // GET all alerts page
-// TODO change to get 10 at a time with additional parameter
+// TODO change to get 10 at a time with paging
 router.get('/alerts', (req, res) => {
-  getAllAlerts()
+  db.getAllAlerts()
     .then(alerts => res.json(alerts))
     .catch(err => console.log(err))
 })
-// GET one alert
-router.get('/alerts/:aID', (req, res) => {
-  getOneAlert(req.params.aID)
-    .then(alert => res.json(alert))
-})
+
 // POST new alert
 router.post('/alerts', (req, res) => {
-  addNewAlert(req.body)
-    .then(alerts => res.json(alerts))
+  db.addNewAlert(req.body)
+    .then((response) => {
+      console.log('response', response);
+      db.getAllAlerts()
+        .then(alerts => res.status(201).json(alerts))
+    })
+    .catch(err => console.log(err))
 })
+
 // GET filter/search alerts
 // TODO modularize into db.js file and make fuzzy
-router.get('/alerts/search', (req, res, next) => {
-  console.log(req.query.terms);
-  const terms = req.query.terms
-  Alert
-    .where({$or: [{title: { $in: [terms]}}]})
+// TODO adapt to use filter as well
+router.get('/alerts/search', (req, res) => {
+  const { terms } = req.query
+  db.filterAlerts(terms)
     .then((alerts) => {
-      console.log(alerts)
-      res.status(201).json(alerts)
+      res.json(alerts)
     })
+    .catch(err => console.log(err))
+})
+
+// GET one alert
+router.get('/alerts/:aID', (req, res) => {
+  db.getOneAlert(req.params.aID)
+    .then(alert => res.json(alert))
+    .catch(err => console.log(err))
 })
 
 // DELETE one alert
-router.delete('/alerts/:aID', (req, res, next) => {
-  deleteOneAlert(req.params.aID)
-    .then(deleted => console.log(deleted))
+router.delete('/alerts/:aID', (req, res) => {
+  db.deleteOneAlert(req.params.aID)
     .then(() => {
-      getAllAlerts()
-        .then(alerts => res.status(201).json(alerts))
+      db.getAllAlerts()
+        .then(alerts => res.json(alerts))
     })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
