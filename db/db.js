4e1bed9419885dfb5f587eee0c7baf05a095aa29
Kelcey Wilson <kelceywilson@gmail.com>
2018-04-27 12:31:53 -0700

    Add filterAlerts query

--- db/db.js
+++ db/db.js
@@ -1,28 +1,19 @@
 const { Alert } = require('../db/models.js')
 
-/*CODE_REVIEW: Consider also adding catch to router that calls these methods
-*/
-
 /**
 * Callback to an express route to get all alerts
 * @return {Promise} - resolves to an array of objects representing all alerts
 */
-const getAllAlerts = () => {
-  return Alert.find({})
-    .sort({createdAt: -1})
-    .catch(err => console.log(err))
-}
+const getAllAlerts = () => Alert.find({}).sort({createdAt: -1})
 
 /**
 * Callback to an express route to add a new alert
 * @param {object} newAlertPost - represents new alert
-* @return {Promise} -resolves to an array of objects representing all alerts
+* @return {Promise} -resolves to an object representing the alert
 */
-const addNewAlert = (newAlertPost, next) => {
+const addNewAlert = (newAlertPost) => {
   const newAlert = new Alert(newAlertPost)
   return newAlert.save()
-    .then(() => getAllAlerts())
-    .catch(err => next(err))
 }
 
 /**
@@ -30,26 +21,30 @@ const addNewAlert = (newAlertPost, next) => {
  * @param  {string} alertId - unique id of alert
  * @return {Promise} - resolves to an object representing the alert
  */
-const getOneAlert = (alertId) => {
-  return Alert.findById(alertId)
-    .catch(err => console.log(err))
-}
+const getOneAlert = alertId => Alert.findById(alertId)
 
 /**
  * Callback to an express route to delete an alert of given id
  * @param  {string} alertId - unique id of alert
- * @return {Promise} -resolves to an array of objects representing all alerts
+ * @return {Promise} - resolves to an array of objects representing all alerts
  */
-const deleteOneAlert = (alertId) => {
-  return Alert.findById(alertId, (err, alertToDelete) => {
-    alertToDelete.remove()
-  })
-    .catch(err => console.log(err))
+const deleteOneAlert = alertId => Alert.findByIdAndRemove(alertId)
+
+// TODO tweak to search body, price, etc., rather than type
+/**
+ * Callback to an express route to search for all alerts that has search terms in title or type
+ * @param  {string} terms - entered search terms
+ * @return {Promise} - resolves to array of objects representing relevant alerts
+ */
+const filterAlerts = (terms) => {
+  const splitTerms = terms.split(' ')
+  return Alert.where({$or: [{title: { $in: splitTerms}}, {type: { $in: splitTerms}}]})
 }
 
 module.exports = {
   addNewAlert,
   deleteOneAlert,
+  filterAlerts,
   getAllAlerts,
   getOneAlert
 }
