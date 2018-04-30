c30bf6d4832a4ac88467a784a8f29e918967d6c5
Kelcey Wilson <kelceywilson@gmail.com>
2018-04-30 11:38:52 -0700

    Add authorization to Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+// const cors = require('cors')
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/alerts')
 
@@ -15,6 +16,7 @@ const app = express()
 app.set('port', (process.env.PORT || 5000))
 
 app.use(morgan('dev'))
+// app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({type: '*/*'}))
 
@@ -29,11 +31,10 @@ db.once('open', () => {
   console.log('db connection successful');
 })
 
-// this could all be replaced by installing cors middleware
-// and then adding app.use(cors())
+// this can all be replaced by uncommenting cors middleware and app.use
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization')
   if(req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE')
     return res.status(200).json({})
